739426be13e7c4a568d9ea8060daefcc12b8303d
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-02 00:42:11 +0200

    Vertical tabs for in page navigation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>ePharmacy Delivery System</h1>
       <Dashboard />
     </div>
   );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,100 @@
-import React, { Component } from 'react'
-import PharmacyItem from './Pharmacy/PharmacyItem';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Dashboard</h1>
-                <PharmacyItem />
-            </div>
-        )
-    }
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={4}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
-export default Dashboard;
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 224,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
+
+export default function Dasboard() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <Tabs
+                orientation="vertical"
+                value={value}
+                variant={"fullWidth"}
+                scrollButtons={"off"}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+            >
+                <Tab label="Pharmacy" {...a11yProps(0)} />
+                <Tab label="Bank Account" {...a11yProps(1)} />
+                <Tab label="Driver" {...a11yProps(2)} />
+                <Tab label="Vehicle" {...a11yProps(3)} />
+                <Tab label="Medicine" {...a11yProps(4)} />
+                <Tab label="Orders" {...a11yProps(5)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Item Four
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+                Item Five
+            </TabPanel>
+            <TabPanel value={value} index={5}>
+                Item Six
+            </TabPanel>
+        </div>
+    );
+}
