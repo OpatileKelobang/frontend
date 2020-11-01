e173bf95dd02fc0564fcaee542a3173ea2393cf8
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-01 23:53:49 +0200

    modified test class to take snap shot

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import {render} from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { asFragment  } = render(<App />);
-  expect(asFragment(<App />)).toMatchSnapshot();
+it('should take a snapshot', () => {
+  const { asFragment } = render(<App />)
+
+  expect(asFragment(<App />)).toMatchSnapshot()
 });
